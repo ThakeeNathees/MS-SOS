aaa3e1d051be5814f320bc0f69b1dc53ab0034d1
Thakee Natheest <41085900+ThakeeNathees@users.noreply.github.com>
2019-04-09 21:35:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ if [ "$1" = "start" ]; then
 
   echo "[auto-builder] building kernel"
   gcc -m32 -o ./kernel/kernel.o -c ./kernel/kernel.c -nostdlib -nostartfiles -nodefaultlibs
+  gcc -m32 -o ./kernel/shell.o -c ./kernel/shell.c -nostdlib -nostartfiles -nodefaultlibs
   gcc -m32 -o ./drivers/screen.o -c ./drivers/screen.c -nostdlib -nostartfiles -nodefaultlibs
   gcc -m32 -o ./drivers/ports.o -c ./drivers/ports.c -nostdlib -nostartfiles -nodefaultlibs
   gcc -m32 -o ./drivers/cursor.o -c ./drivers/cursor.c -nostdlib -nostartfiles -nodefaultlibs
@@ -12,11 +13,12 @@ if [ "$1" = "start" ]; then
   gcc -m32 -o ./drivers/keyboard/keyhandler.o -c ./drivers/keyboard/keyhandler.c -nostdlib -nostartfiles -nodefaultlibs
 
   echo "[auto-builder] linking with linker.ld"
-  ld -T ./boot/linker.ld -m elf_i386 -o os.bin ./boot/boot.o ./drivers/cursor.o ./drivers/keyboard/keyhandler.o ./drivers/keyboard/keyboard.o ./drivers/screen.o ./drivers/ports.o ./kernel/kernel.o
+  ld -T ./boot/linker.ld -m elf_i386 -o os.bin ./boot/boot.o ./drivers/cursor.o ./drivers/keyboard/keyhandler.o ./drivers/keyboard/keyboard.o ./drivers/screen.o ./drivers/ports.o ./kernel/kernel.o ./kernel/shell.o
   
   echo "[auto-bulider] cleaning"
   rm ./boot/boot.o
   rm ./kernel/kernel.o
+  rm ./kernel/shell.o
   rm ./drivers/screen.o
   rm ./drivers/ports.o
   rm ./drivers/cursor.o
